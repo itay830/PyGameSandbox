3894296b94ab04bb84308553f8ea94ecd9506d8b
Itay <itay.shinderman@gmail.com>
2023-04-01 11:50:05 +0300

    Added better button and game managements.

--- buttonClass.py
+++ buttonClass.py
@@ -5,19 +5,33 @@ pygame.init()
 
 
 class Button:
-    def __init__(self, pos, width, height, color, txt_properties: dict = None):
+    buttons = []
+
+    def __init__(self, pos, width, height, color, txt_properties: dict = None, selector_button: bool = False):
         self.surf = pygame.Surface((width, height))
         self.surf.fill(color)
         self.rect = self.surf.get_rect(center=pos)
         self.clickStage = None
 
         if txt_properties is not None:
+            if selector_button:
+                txt_properties['pos'] = (self.rect.centerx, self.rect.centery + 75)
             self.txtSurf, self.txtRect = create_txt(txt_properties.get('font'), txt_properties.get('size', 0),
                                                     txt_properties.get('bald', False),
                                                     txt_properties.get('italic', False), txt_properties.get('txt', ''),
                                                     txt_properties.get('antialias', False),
                                                     txt_properties.get('color', (0, 0, 0)),
                                                     txt_properties.get('pos', (0, 0)))
+
+        Button.buttons.append(self)
+
+    def add_txt(self, txt_properties: dict):
+        self.txtSurf, self.txtRect = create_txt(txt_properties.get('font'), txt_properties.get('size', 0),
+                                                txt_properties.get('bald', False),
+                                                txt_properties.get('italic', False), txt_properties.get('txt', ''),
+                                                txt_properties.get('antialias', False),
+                                                txt_properties.get('color', (0, 0, 0)),
+                                                txt_properties.get('pos', (0, 0)))
 
     def draw(self, draw_surf):
         draw_surf.blit(self.surf, self.rect)
--- main.py
+++ main.py
@@ -2,45 +2,46 @@ from sys import exit
 from buttonClass import Button
 import pygame, math, time
 
-screen = pygame.display.set_mode((0, 0), flags=pygame.FULLSCREEN)
-WIDTH, HEIGHT = screen.get_width(), screen.get_height()
-clock = pygame.time.Clock()
 
-
-demoDict = {
-    'font': 'comicsansms',
-    'size': 25,
-    'bald': True,
-    'italic': True,
-    'txt': "A demo button",
-    'antialias': True,
-    'color': (255, 255, 255),
-    'pos': (WIDTH / 2, HEIGHT / 2 + 75)
-}
-demo = Button((WIDTH / 2, HEIGHT / 2), 100, 100, (255, 0, 126), demoDict)
-
-
-def mainloop():
-    FPS = 60
-    previousTime = time.time()
-    while 1:
-        nowTime = time.time()
-        speedCallibaration = (nowTime - previousTime) * FPS
-        previousTime = nowTime
-
+class App:
+    @staticmethod
+    def event_handler():
         screen.fill((0, 0, 0))
         clock.tick(FPS)
-
         for event in pygame.event.get():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     exit()
 
-        demo.update(screen)
 
-        pygame.display.update()
 
 
 if __name__ == '__main__':
-    mainloop()
+    screen = pygame.display.set_mode((0, 0), flags=pygame.FULLSCREEN)
+    WIDTH, HEIGHT = screen.get_width(), screen.get_height()
+    clock = pygame.time.Clock()
+    FPS = 60
+    previousTime = time.time()
+
+    tileMapButton = {
+        'font': 'comicsansms',
+        'size': 25,
+        'bald': True,
+        'italic': True,
+        'txt': "TileMapMaker",
+        'antialias': True,
+        'color': (255, 255, 255),
+    }
+    demo = Button((100, 75), 100, 100, (255, 0, 126), tileMapButton, True)
+
+    while 1:
+        nowTime = time.time()
+        speedCallibaration = (nowTime - previousTime) * FPS
+        previousTime = nowTime
+        App.event_handler()
+
+        for button in Button.buttons:
+            button.update(screen)
+
+        pygame.display.update()
